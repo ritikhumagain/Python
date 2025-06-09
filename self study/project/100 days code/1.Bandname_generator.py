9b496a42a4076abde93b93d77e5236f4a70e58a4
Ritik Humagain <ritikhumagain@gmail.com>
2025-06-09 11:16:31 +0545

    1st day

---
+++ self study/project/100 days code/1.Bandname_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator ")
+name_city = str(input('Whats name of the city you grew up in ?'))
+name_pet = str(input("What's your pet's name ?"))
+print((((((f"Your band name could be :{name_city} {name_pet}"))))))
