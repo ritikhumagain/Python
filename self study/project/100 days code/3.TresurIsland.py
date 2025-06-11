c635af0087cb1747522def15527a3c26b0d8d08d
Ritik Humagain <ritikhumagain@gmail.com>
2025-06-11 12:01:48 +0545

    bro code

---
+++ self study/project/100 days code/3.TresurIsland.py
@@ -0,0 +1,9 @@
+# print('Welcome to Treausr Island.\nYour mission is to find the treasur. \nYoure at a cross road. Where do you want to go ? Type "left" or "right "')
+# a = input('chose between left or right')
+# if a == "left":
+#     print("you have reached your destination ")
+# else:
+#     print(you come to a lake . there is an island in the middle of the lake. type 'wait 'to wait for a boat .type swim to swim acrr )
+
+
+
